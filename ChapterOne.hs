2681e4b90d3678585069b191d19cd06eb94bbc15
Dezmon Fernandez <defernan@mines.edu>
2015-02-27 02:12:13 -0700

    now tracked

--- ChapterOne.hs
+++ ChapterOne.hs
@@ -1,1 +1,40 @@
-a
+module ChapterOne where
+
+
+main = print( negsInList [1,-1,2] )
+	print( negsInList [] )
+	print( negsInList [1,0,3,4,5] )
+	print( negsInList [-21,-1,-2])
+
+--negsInList: 
+negsInList xs = [ x | x <- xs && x > 0 ]
+
+{-
+oddsInList =
+
+
+capsInList =
+
+
+sumOdd = 
+
+
+getQuantities = 
+
+getPrices =
+
+
+getTotal =
+
+
+doubleAll =
+
+
+doubleFirstList =
+
+
+doubleSecondList =
+
+sumListItems =
+
+-}
